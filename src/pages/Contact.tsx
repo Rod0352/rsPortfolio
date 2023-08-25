bf6f6e622189265fe041cb79acea443a603e4c34
Rod3052 <rscott@westell.com>
2023-08-24 23:19:33 -0400

    Starting on contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -13,12 +13,83 @@ export const Contact = () => {
         // justifyContent: "center",
       }}
     >
-      <Typography variant="h3">Contact</Typography>
-      <FormControl>
-        <TextField sx={{ color: "#fff" }} label="Name" />
-        <TextField label="Email" />
-        <TextField label="Message" />
-      </FormControl>
+      <Typography variant="h3">Lets create something together!</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          flexDirection: "column",
+          color: "red",
+        }}
+      >
+        <FormControl
+          sx={{
+            width: "50%",
+            margin: "1rem",
+          }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Name"
+            variant="outlined"
+            color="secondary"
+            sx={{
+              width: "100%",
+            }}
+          />
+        </FormControl>
+        <FormControl
+          sx={{
+            width: "50%",
+            margin: "1rem",
+          }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            color="secondary"
+            sx={{
+              width: "100%",
+            }}
+          />
+        </FormControl>
+        <FormControl
+          sx={{
+            width: "50%",
+            margin: "1rem",
+          }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Message"
+            variant="outlined"
+            color="secondary"
+            sx={{
+              width: "100%",
+            }}
+          />
+        </FormControl>
+
+        <FormControl
+          sx={{
+            width: "50%",
+            margin: "1rem",
+          }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Phone"
+            variant="outlined"
+            color="secondary"
+            sx={{
+              width: "100%",
+            }}
+          />
+        </FormControl>
+      </Box>
     </Box>
   );
 };
